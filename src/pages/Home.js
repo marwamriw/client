b03cf78fe6d114b63b7f1672980ef6ca332e1529
marwa <boukhrismarwa06@gmail.com>
2024-01-26 09:34:22 +0100

    FirstComit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <h1>THIS IS MY HOME PAGE</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+import rootReducer from "./slices/userSlice";
+
+export default configureStore ({reducer:{user: rootReducer}})
